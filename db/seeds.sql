9f58fdd2b1e60933371fbe8db0e044701ba6de85
C.S Wolf <37349534+CSneed7@users.noreply.github.com>
2018-08-23 16:44:07 -0400

    adding all files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('Triple Stack Burger');
+INSERT INTO burgers (burger_name) VALUES ('Donut Burger');

f7daeadfaecf7006f89d94b743c498e09dbee3c0
C.S Wolf <37349534+CSneed7@users.noreply.github.com>
2018-08-23 18:41:56 -0400

    made changes, made it better

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 var app = express();
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 var handlebars = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burger_controller.js");
